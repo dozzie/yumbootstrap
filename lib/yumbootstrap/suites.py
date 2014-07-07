96964fa67115d4082b4ecf3af92fe9c6b4201ab8
Stanislaw Klekot <dozzie@jarowit.net>
2014-07-07 18:48:08 +0200

    Reorganized yumbootstrap.suites module.

--- lib/yumbootstrap/suites.py
+++ lib/yumbootstrap/suites.py
@@ -33,77 +33,215 @@ def load_suite(directory, suite_name):
 
 #-----------------------------------------------------------------------------
 
-class Section:
-  def __init__(self, values):
-    self._values = values
+# generic section
+class Section(object):
+  LINE = re.compile(
+    r'^(?P<name>[a-zA-Z0-9_-]+)\s*=\s*(?P<value>\S.*|)$'
+  )
+
+  def __init__(self):
+    self._values = {}
+
+  def add(self, name, value):
+    if name not in self._values:
+      self._values[name] = []
+    self._values[name].append(value)
 
   def __iter__(self):
     return self._values.__iter__()
 
   def __contains__(self, name):
-    return name in self._values
+    return (name in self._values)
 
   def __getitem__(self, name):
     if name in self._values:
-      return self.get_all(name)[0]
+      return self._values[name][0]
 
   def get_all(self, name):
     return self._values.get(name, [])
 
+  def dict(self):
+    return self._values.copy()
+
 #-----------------------------------------------------------------------------
 
+# top-level, unnamed section
+# two keys get special treatment
+class MainSection(Section):
+  LINE = re.compile(
+    r'^(?P<name>[a-zA-Z0-9_-]+)\s*(?P<set>\??=)\s*(?P<value>\S.*|)$'
+  )
+
+  def __init__(self, directory, suite):
+    super(MainSection, self).__init__()
+    self._directory = directory
+    self._suite = suite
+
+  def add(self, name, set, value):
+    if name in ('packages', 'gpg_key'):
+      # expand "${suite}"
+      value = value.replace('${suite}', self._suite)
+      # make the file relative to suite file location
+      value = os.path.normpath(os.path.join(self._directory, value))
+      if set == '?=' and not os.path.exists(value):
+        return
+    super(MainSection, self).add(name = name, value = value)
+
+#-----------------------------------------------------------------------------
+
+# [repositories] section
+# repos.dict() only returns first element from the list
+class RepositorySection(Section):
+  def dict(self):
+    return dict([(name, value[0]) for (name, value) in self._values.items()])
+
+#-----------------------------------------------------------------------------
+
+# [post_install] section
+# remembers order between all the entries, not only in group
+class ScriptSection(Section):
+  LINE = re.compile(
+    r'^(?P<name>[a-zA-Z0-9_-]+)\s*(?P<set>\??=)\s*(?P<value>\S.*|)$'
+  )
+
+  def __init__(self, directory, suite):
+    self._scripts_global = [] # ('name', ['script', ...])
+    self._scripts = {}
+    self._directory = directory
+    self._suite = suite
+
+  def add(self, name, set, value):
+    # expand "${suite}"
+    value = value.replace('${suite}', self._suite)
+
+    script = value.split(' ')
+    # make the path relative to suite file location (if applicable)
+    if '/' in script[0]:
+      script[0] = os.path.normpath(os.path.join(self._directory, script[0]))
+
+    # skip the entry if it's conditional and the target file doesn't exist
+    if set == '?=' and '/' in script[0] and not os.path.exists(script[0]):
+      return
+
+    self._scripts_global.append((name, script))
+    if name not in self._scripts:
+      self._scripts[name] = []
+    self._scripts[name].append(script)
+
+  def names(self):
+    return sorted(self._scripts.keys())
+
+  def __iter__(self):
+    for (name, script) in self._scripts_global:
+      yield script
+
+  def skip(self, names):
+    if isinstance(names, (list, tuple, dict)):
+      names = set(names)
+    else:
+      names = set([names])
+
+    for (name, script) in self._scripts_global:
+      if name not in names:
+        yield script
+
+  def just(self, names):
+    if isinstance(names, (list, tuple, dict)):
+      names = set(names)
+    else:
+      names = set([names])
+
+    for (name, script) in self._scripts_global:
+      if name in names:
+        yield script
+
+  def __contains__(self, name):
+    return (name in self._scripts)
+
+  def __getitem__(self, name):
+    return self._scripts.get(name)
+
+#-----------------------------------------------------------------------------
+
+# [environment] section
+class EnvironmentSection(Section):
+  LINE = re.compile(
+    '^(?:'
+      '(?P<variable>'
+        r'(?P<name>[a-zA-Z0-9_-]+)\s*=\s*(?P<value>\S.*|)'
+      ')|(?P<keep>'
+        r'(?P<keep_name>[a-zA-Z0-9_*]+)'
+      ')'
+    ')$'
+  )
+
+  def __init__(self):
+    self._set = {}
+    self._keep = []
+
+  def add(self, variable, name, value, keep, keep_name):
+    if keep_name is not None:
+      self._keep.append(keep_name)
+    elif name not in self._set:
+      self._set[name] = value
+    # else silently discard (keep only the first occurrence)
+
+  def __iter__(self):
+    raise NotImplementedError() # TODO
+
+  def __contains__(self, name):
+    raise NotImplementedError() # TODO
+
+  def __getitem__(self, name):
+    raise NotImplementedError() # TODO
+
+  def dict(self):
+    raise NotImplementedError() # TODO
+
+#-----------------------------------------------------------------------------
+
+# packages list
 class Packages:
   def __init__(self):
     self.install = []
     self.groups = []
     self.exclude = []
 
-  def add_file(self, filename):
+  def read(self, filename):
     f = open(filename)
     while True:
       line = f.readline()
       if line == '':
         return
-
       line = line.strip()
       if line == '' or line.startswith("#"):
         continue
+      self.add(line)
 
-      if line.startswith('-'):
-        self.exclude.append(line[1:])
-      elif line.startswith('@'):
-        self.groups.append(line[1:])
-      else:
-        self.install.append(line)
-
-#-----------------------------------------------------------------------------
-
-_CONFIG_LINE_RE = re.compile(
-  '^(?:'
-    '(?P<variable>'
-      r'(?P<name>[a-zA-Z0-9_-]+)\s*(?P<set>\??=)\s*(?P<value>\S.*\S|\S|)'
-    ')|(?P<section>'
-      r'\[(?P<section_name>[a-zA-Z0-9_-]+)\]'
-    ')|(?P<env_keep>'
-      r'(?P<env_name>[a-zA-Z0-9_*]+)'
-    ')'
-  ')\s*$'
-)
+  def add(self, line):
+    if line.startswith('-'):
+      self.exclude.append(line[1:])
+    elif line.startswith('@'):
+      self.groups.append(line[1:])
+    else:
+      self.install.append(line)
 
 #-----------------------------------------------------------------------------
 
 class Suite:
   def __init__(self, suite, filename):
+    self._directory = os.path.abspath(os.path.dirname(filename))
     self._suite = suite
-    self._sections = {}
-    self._read(filename)
-    if 'environment' not in self._sections:
-      self._sections['environment'] = Section({})
-    if 'repositories' not in self._sections:
-      self._sections['repositories'] = Section({})
+    self._main = MainSection(self._directory, suite)
     self._packages = Packages()
+    self._environment = EnvironmentSection()
+    self._repositories = RepositorySection()
+    self._post_install = ScriptSection(self._directory, suite)
+    self._sections = {} # unrecognized ones
+    self.read(filename)
+
     for f in self.get_all('packages'):
-      self._packages.add_file(f)
+      self._packages.read(f)
 
   #---------------------------------------------------------
 
@@ -125,31 +263,27 @@ class Suite:
 
   @property
   def repositories(self):
-    repos = self._sections['repositories']
-    return dict([(name, repos[name]) for name in repos])
+    return self._repositories.dict()
 
   @property
   def post_install(self):
-    # TODO: this should be a sequence of scripts in the same order as in suite
-    # file
-    return self._sections['post_install']
+    return self._post_install
 
   @property
   def environment(self):
-    # TODO: this should be a hash with proper values
-    return self._sections['environment']
+    return self._environment.dict()
 
   #---------------------------------------------------------
 
   def __contains__(self, name):
-    return name in self._sections[None]
+    return (name in self._main)
 
   def __getitem__(self, name):
-    if name in self._sections[None]:
-      return self._sections[None][name]
+    if name in self._main:
+      return self._main[name]
 
   def get_all(self, name):
-    return self._sections[None].get_all(name)
+    return self._main.get_all(name)
 
   def section(self, name):
     if section in self._sections:
@@ -157,96 +291,48 @@ class Suite:
 
   #---------------------------------------------------------
 
-  def _read(self, filename):
+  def read(self, filename):
     f = open(filename)
-    directory = os.path.abspath(os.path.dirname(filename))
     lineno = 0
-    section = None
-    values = {}
+    line = None
+    section = self._main # start with main section
+    section_name = None
 
-    def error(line):
-      if section is None:
-        raise YBError('Invalid config line %d: %s', lineno, line, exit = 1)
-      else:
-        raise YBError('Invalid config line %d (section %s): %s',
-                      lineno, section, line, exit = 1)
 
     while True:
       line = f.readline()
-      lineno += 1
       if line == '':
-        self._sections[section] = Section(values)
         return
       line = line.strip()
+      lineno += 1
 
       if line == '' or line.startswith("#"):
         continue
-      match = _CONFIG_LINE_RE.match(line)
-      if match is None: error(line)
+
+      if line[0] == '[' and line[-1] == ']':
+        section_name = line[1:-1]
+        if section_name == 'repositories':
+          section = self._repositories
+        elif section_name == 'environment':
+          section = self._environment
+        elif section_name == 'post_install':
+          section = self._post_install
+        elif section_name in self._sections:
+          section = self._sections[section_name]
+        else:
+          section = self._sections[section_name] = Section()
+        continue
+
+      match = section.LINE.match(line)
+      if not match:
+        if section_name is None:
+          raise YBError('Invalid config line %d: %s', lineno, line, exit = 1)
+        else:
+          raise YBError('Invalid config line %d (section %s): %s',
+                        lineno, section_name, line, exit = 1)
 
       groups = match.groupdict()
-
-      if groups['section'] is not None:
-        # new section
-        self._sections[section] = Section(values)
-        section = groups['section_name']
-        if section == 'environment':
-          values = {'keep': [], 'set': {}}
-        else:
-          values = {}
-        continue
-
-      if section == 'environment':
-        # variable and env_keep are OK, rest is not
-        if groups['variable'] is not None:
-          if groups['name'] not in values['set']:
-            values['set'][ groups['name'] ] = []
-          values['set'][ groups['name'] ].append(groups['value'])
-        elif groups['env_keep'] is not None:
-          values['keep'].append(groups['env_name'])
-        else:
-          error(line)
-        continue
-
-      if section == 'repositories' and groups['set'] != '=':
-        # variable has to be non-conditional in repositories
-        error(line)
-
-      if section != 'environment' and groups['env_keep'] is not None:
-        # env_keep line is only allowed in environment section
-        error(line)
-
-      # substitute ${suite} in groups['value']
-      if section not in ('repositories', 'environment'):
-        groups['value'] = groups['value'].replace('${suite}', self._suite)
-
-      if section is None and groups['variable'] is not None and \
-         groups['name'] in ('gpg_key', 'packages'):
-        # replace relative path in option with absolute path (paths are
-        # relative to the suite file location)
-        if not groups['value'].startswith('/'):
-          groups['value'] = os.path.join(directory, groups['value'])
-          groups['value'] = os.path.normpath(groups['value'])
-
-      if section == 'post_install' and not groups['value'].startswith('/'):
-        # make the script relative to suite file location, but only if it's
-        # in a form of a path already (don't change $PATH searches)
-        script = groups['value'].split(' ')
-        if '/' in script[0]:
-          script[0] = os.path.join(directory, script[0])
-          script[0] = os.path.normpath(script[0])
-          groups['value'] = ' '.join(script)
-
-      if groups['set'] == '?=' and \
-         not os.path.exists(groups['value'].split(' ')[0]):
-        # skip conditional if the file does not exist
-        continue
-
-      # any other section
-      # variable is OK, rest is not
-      if groups['name'] not in values:
-        values[ groups['name'] ] = []
-      values[ groups['name'] ].append(groups['value'])
+      section.add(**groups)
 
 #-----------------------------------------------------------------------------
 # vim:ft=python
