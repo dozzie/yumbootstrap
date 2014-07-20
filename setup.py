1e9620bd161b33291f128fe0a8aeb45e12e0f7ce
Stanislaw Klekot <dozzie@jarowit.net>
2014-07-20 15:34:04 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from glob import glob
 
 setup(
   name         = 'yumbootstrap',
-  version      = '0.0.1',
+  version      = '0.0.2',
   description  = 'chroot installer for Red Hat derivatives',
   #scripts      = glob("bin/*"), # yumbootstrap should go to sbin, not to bin
   packages     = find_packages("lib"),
